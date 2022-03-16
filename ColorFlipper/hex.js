b034f2b6da51753b229e45925afce72a6ed76829
Ilya <ilya@sinyugin.ru>
2022-03-16 19:34:52 +0100

    made a new function and added a few buttons in hex implementation

--- ColorFlipper/hex.js
+++ ColorFlipper/hex.js
@@ -1,16 +1,43 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
+const btn2 = document.getElementById("btn2");
+const btn3 = document.getElementById("btn3");
+
 
 btn.addEventListener('click', function () {
+    let randomColor = buildRandomColor();
+    color.textContent = randomColor;
+    document.body.style.backgroundColor = randomColor;
+});
+
+btn2.addEventListener('click', function () {
+    let blackColor = '#000000';
+    color.textContent = blackColor;
+    document.body.style.backgroundColor = '#000000';
+});
+
+btn3.addEventListener('click', function () {
+    let whiteColor = '#ffffff';
+    color.textContent = whiteColor;
+    document.body.style.backgroundColor = '#ffffff';
+});
+
+// btn4.addEventListener('click', function () {
+//     let hexColor = '#'
+//     for (let i = 0; i < 6; i++) {
+//         hexColor += hex[getRandomNumber()];
+//     }
+// });
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length);
+}
+
+function buildRandomColor() {
     let hexColor = '#'
     for (let i = 0; i < 6; i++) {
         hexColor += hex[getRandomNumber()];
     }
-    color.textContent = hexColor;
-    document.body.style.backgroundColor = hexColor;
-});
-
-function getRandomNumber() {
-    return Math.floor(Math.random() * hex.length);
+    return hexColor;
 }
